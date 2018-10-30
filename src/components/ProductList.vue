ef01273cbece700185560a6ca44d2740ca82e125
iarafat <miyasinarafat@gmail.com>
2018-10-31 00:17:13 +0600

    lesson 6, components and the vue instance

---
+++ src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="products">
+      ...
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      created () {
+        if (this.products.length === 0) {
+          this.$store.dispatch('allProducts')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+.products {
+  background: #F7F8FB;
+  padding: 30px 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,45 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+/*new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+})*/
+
+/*new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: `
+        <div>
+            <p>{{counter}}</p>
+            <button @click="incrementCounter">++</button>
+        </div>
+    `,
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++
+    }
+  }
+})*/
+
+const GreetingComponent = {
+  template: `<h1>Hi, you!</h1>`,
+}
+
+new Vue({
+  el: '#app',
+  template: `
+    <div>
+        <GreetingComponent/>
+    </div>
+  `,
+  components: { GreetingComponent },
+});
